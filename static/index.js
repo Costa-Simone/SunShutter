849f4ba3db44bea4045f728473a1cb51cf384c5b
Costa Simone <s.costa.2235@vallauri.edu>
2024-03-01 17:26:07 +0100

    Creazione dell'input per l'apertura

--- static/index.js
+++ static/index.js
@@ -1,23 +1,30 @@
 "use strict"
 
-$(document).ready(function() {
-    let _sectionData = $("#sectionData")
+$(document).ready(function () {
     let _homeButton = $("#homeButton")
     let _settingsButton = $("#settingsButton")
     let _commandsButton = $("#commandsButton")
 
-    let _home = $("#home").show()
-    let _commands = $("#commands").hide()
+    let _sectionData = $("#sectionData").hide()
+    let _commands = $("#commands").show()
+
+    let _switchOpen = $("#toggler-1").prop("disabled", true)
 
     let serverSocket = io.connect("http://localhost:3000")
 
-    serverSocket.on('connect', function() {console.log("connessione ok")} )
-    serverSocket.on("chat message", data => {console.log(data)})
+    let isOpen = false
+
+    serverSocket.on('connect', function () { console.log("connessione ok") })
+    serverSocket.on("chat message", data => { console.log(data) })
 
     GetData()
 
-    _homeButton.on("click", function() {
-        _home.show()
+    _switchOpen.on("click", function () {
+        isOpen = !isOpen
+    })
+    
+    _homeButton.on("click", function () {
+        _sectionData.show()
         _commands.hide()
 
         _homeButton.addClass("active")
@@ -25,8 +32,8 @@ $(document).ready(function() {
         _settingsButton.removeClass("active")
     })
 
-    _commandsButton.on("click", function() {
-        _home.hide()
+    _commandsButton.on("click", function () {
+        _sectionData.hide()
         _commands.show()
 
         _homeButton.removeClass("active")
@@ -34,36 +41,38 @@ $(document).ready(function() {
         _settingsButton.removeClass("active")
     })
 
-    _settingsButton.on("click", function() {
-        _home.hide()
+    _settingsButton.on("click", function () {
+        _sectionData.hide()
         _commands.hide()
 
         _homeButton.removeClass("active")
         _commandsButton.removeClass("active")
         _settingsButton.addClass("active")
     })
-    
+
     function GetData() {
         let rq = inviaRichiesta("GET", "/api/sunshutter")
 
         rq.catch(errore)
         rq.then(response => {
-            console.log(response)
-            for(let key in response["data"][0]) {
+            for (let key in response["data"][0]) {
                 let title
                 let text
 
-                switch(key) {
+                switch (key) {
                     case "Apertura":
                         title = "Apertura"
 
-                        if(response["data"][0][key] == false) {
+                        if (response["data"][0][key] == false) {
                             text = "Chiuso"
+                            isOpen = false
                         } else {
                             text = "Aperto"
+                            _switchOpen.prop("disabled", false).trigger("click")
+                            isOpen = true
                         }
                         break
-                    
+
                     case "Consumo":
                         title = "Consumo"
                         text = `${response["data"][0][key]["$numberDecimal"]} W`
@@ -83,7 +92,7 @@ $(document).ready(function() {
                         break;
                 }
 
-                if(title != undefined && text != undefined) {
+                if (title != undefined && text != undefined) {
                     let divCard = $("<div>").addClass("card item").appendTo(_sectionData)
                     let divCardBody = $("<div>").addClass("card-body").appendTo(divCard)
                     $("<h5>").addClass("card-title").text(title).appendTo(divCardBody).appendTo(divCardBody)
